da0916db4614bdc3605bd9bf982ecf643ef3fb33
Sarah Layane <259081865+eduardoaugustoalcantara-code@users.noreply.github.com>
2026-03-19 12:12:03 -0300

    aula de hoje

---
+++ atividade_03/cinema.js
@@ -0,0 +1,10 @@
+let assentos = [
+    ["L","L","L"],
+    ["L","O","L"],
+    ["O","L","L"]
+];
+
+let cadeirameio = asssentos[1][1];
+console.log("a cadeira do meio está" + cadeirameio);
+assentos[0][0] = "O";
+console.timeLog("O assentos em [0][0] está " + assentos[0][0])
---
+++ atividade_03/lista_compras.js
@@ -0,0 +1,8 @@
+let lista_compras = ["arroz, feijão, açúcar,salmão"];
+lista_compras.forEach(item => {
+    console.log(item);
+});
+
+let cesta_frutas = ["maçã, manga, uva, limão"];
+lista_compras.push("macarrão");
+lista_compras.forEach(item);
